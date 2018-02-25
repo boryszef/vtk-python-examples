f2bbb7cdfd8fc1ea353904918df4ba01aa3b1440
Borys Szefczyk <boryszef@gmail.com>
2018-02-25 15:57:08 +0100

    Interactor example

---
+++ interactor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import vtk
+
+cone = vtk.vtkConeSource()
+
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputConnection(cone.GetOutputPort())
+
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+
+renderer = vtk.vtkRenderer()
+renderer.AddActor(actor)
+renderer.ResetCamera()
+
+window = vtk.vtkRenderWindow()
+window.AddRenderer(renderer)
+
+istyle = vtk.vtkInteractorStyleSwitch()
+istyle.SetCurrentStyleToTrackballCamera()
+
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetRenderWindow(window)
+interactor.SetInteractorStyle(istyle)
+
+interactor.Initialize()
+interactor.Start()
+
